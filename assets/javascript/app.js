37fd69fe294c5826fc57b4bd681959e372b5e54a
Lee-Dillon <lee.dillon1@outlook.com>
2023-02-02 16:59:27 +0000

    now working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
     var searchString = "";
     var queryURL;
     var articleNumber = 0;
-        
-    https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
-    
+
+    // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+
     $(".clear").click(function () {
         articleNumber = 0;
         $("#search-string").val("");
         $("#article-results").empty();
     });
-    
+
     $(".search").on("click", function () {
         $("#article-results").empty();
         articleNumber = 0;
@@ -24,24 +24,27 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         }).then(function (result) {
-            console.log(result);
-            console.log(result.response.docs);
-            console.log(result.response.docs.length);
+            // console.log(result);
+            // console.log(result.response.docs);
+            // console.log(result.response.docs.length);
+            // console.log(result.response.docs[0].headline.main)
+            // console.log(result.response.docs[0].abstract)
+            // console.log(result.response.docs[0])
             for (i = 0; i < result.response.docs.length; i++) {
                 articleNumber++;
                 var article = $("<div>");
                 article.addClass("well well-lg row");
                 var title = $("<h3>");
                 title.addClass("title");
-                title.text(result.docs[i].headline.main);
+                title.text(result.response.docs[i].headline.main);
                 var description = $("<p>");
                 description.addClass("description");
-                description.text(result.docs[i].abstract);
+                description.text(result.response.docs[i].abstract);
                 var number = $("<div class='articleNumber'>").text(articleNumber);
                 $(article).append(number, title, description);
                 $("#article-results").append(article);
-                console.log(result.docs[i].headline.main)
-                console.log(result.docs[i].abstract)
+                // console.log(result.response.docs[i].headline.main)
+                // console.log(result.response.docs[i].abstract)
             }
         });
     });
