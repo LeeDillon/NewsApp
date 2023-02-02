5007dc78cac452f698b1a2d8aa3ebf2cdbc6326b
Iwona Malkinska <iwonamalkinska@Iwonas-MacBook-Air.local>
2023-02-02 16:52:04 +0000

    added code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
-    var apiURL = "https://gnews.io/api/v4/search?";
-    var key = "&country=us&max=10&token=d06b56befd778f95afde57c26ebc9890";
+    var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    var key = "&api-key=mAREy4OZGu5yDAfpzXgwCZOguhAowON3";
     var searchString = "";
     var queryURL;
     var articleNumber = 0;
         
+    https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
     
     $(".clear").click(function () {
         articleNumber = 0;
@@ -24,20 +25,23 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (result) {
             console.log(result);
-            console.log(result.articles);
-            for (i = 0; i < result.articles.length; i++) {
+            console.log(result.response.docs);
+            console.log(result.response.docs.length);
+            for (i = 0; i < result.response.docs.length; i++) {
                 articleNumber++;
                 var article = $("<div>");
                 article.addClass("well well-lg row");
                 var title = $("<h3>");
                 title.addClass("title");
-                title.text(result.articles[i].title);
+                title.text(result.docs[i].headline.main);
                 var description = $("<p>");
                 description.addClass("description");
-                description.text(result.articles[i].description);
+                description.text(result.docs[i].abstract);
                 var number = $("<div class='articleNumber'>").text(articleNumber);
                 $(article).append(number, title, description);
                 $("#article-results").append(article);
+                console.log(result.docs[i].headline.main)
+                console.log(result.docs[i].abstract)
             }
         });
     });
